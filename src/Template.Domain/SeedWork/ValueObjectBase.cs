371a3f10d377e59e721f11914b2417cdae47f34c
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-03 12:25:14 +0200

    add value object base class

---
+++ src/Template.Domain/SeedWork/ValueObjectBase.cs
@@ -0,0 +1,44 @@
+﻿namespace Template.Domain.SeedWork
+{
+    public abstract class ValueObjectBase : IEquatable<ValueObjectBase>
+    {
+        public abstract IEnumerable<object?> GetEqualityComponents();
+
+        public bool Equals(ValueObjectBase? other)
+        {
+            if (other is null)
+                return false;
+
+            return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public static bool operator== (ValueObjectBase? left, ValueObjectBase? right)
+        {
+            if (left is null && right is null)
+                return true;
+
+            if (left is null || right is null)
+                return false;
+
+            return left.Equals(right);
+        }
+
+        public static bool operator!= (ValueObjectBase? left, ValueObjectBase? right)
+        {
+            return !(left == right);
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ValueObjectBase other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Select(x => x != null ? x.GetHashCode() : 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+    }
+}
