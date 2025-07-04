c2495b60ea7024bf24eaafd08fc79f596031a091
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-04 20:09:51 +0200

    add user entity and its security state

---
+++ src/Template.Domain/Entities/UserManagement/SecurityState.cs
@@ -0,0 +1,18 @@
+﻿using Template.Domain.SeedWork;
+
+namespace Template.Domain.Entities.UserManagement
+{
+    public class SecurityState : EntityBase<Guid>, IUpdateAudit
+    {
+        public string? PasswordHashed { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public int FailedAttempts { get; set; }
+        public bool IsLocked { get; set; }
+        public DateTime UnlockDate { get; set; }
+
+        public DateTime UpdatedDate { get; private set; }
+
+        // Relationships
+        public Guid UserId { get; set; }
+    }
+}
---
+++ src/Template.Domain/Entities/UserManagement/User.cs
@@ -0,0 +1,17 @@
+﻿using Template.Domain.SeedWork;
+
+namespace Template.Domain.Entities.UserManagement
+{
+    public class User : EntityBase<Guid>, IUpdateAudit, ISoftDelete
+    {
+        public string? Email { get; set; }
+
+        public DateTime UpdatedDate { get; private set; }
+
+        public bool IsDeleted { get; private set; }
+        public DateTime DeletedDate { get; private set; }
+
+        // Relationships
+        public SecurityState? SecurityState { get; set; }
+    }
+}
