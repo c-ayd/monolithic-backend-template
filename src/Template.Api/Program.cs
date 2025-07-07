71966abff1e76e508607b66d97ff929e7cb97dfd
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-07 22:17:32 +0200

    add initial migration
    - The related EF Core libraries are added to the persistence and API layers
    - The service registration method is added to the persistence layer

--- src/Template.Api/Program.cs
+++ src/Template.Api/Program.cs
@@ -1,8 +1,12 @@
+using Template.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+//~ Begin - Add services
 builder.Services.AddControllers();
+
+builder.Services.AddPersistenceServices(builder.Configuration);
+//~ End
 
 var app = builder.Build();
 
---
+++ src/Template.Persistence/ServiceRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Template.Persistence.DbContexts;
+using Template.Persistence.Settings;
+
+namespace Template.Persistence
+{
+    public static class ServiceRegistrations
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connStrings = configuration.GetSection(ConnectionStringsSettings.SettingsKey).Get<ConnectionStringsSettings>()!;
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connStrings.App));
+        }
+    }
+}
