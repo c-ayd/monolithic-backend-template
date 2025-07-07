ddf1c52cbe6663e1dfbf8eceb3fd3c45566f92b6
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-07 21:28:36 +0200

    add connection string setting class
    The Cayd.AspNetCore.Settings library is added to automatically map the configurations

---
+++ src/Template.Persistence/Settings/ConnectionStringsSettings.cs
@@ -0,0 +1,12 @@
+﻿using Cayd.AspNetCore.Settings;
+
+namespace Template.Persistence.Settings
+{
+    public class ConnectionStringsSettings : ISettings
+    {
+        public static string SettingsKey => "ConnectionStrings";
+
+        public required string App { get; set; }
+        public string? Test { get; set; }
+    }
+}
