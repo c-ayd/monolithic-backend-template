7b32c0b8289fb35ea504ccff56cc4a1f153690d3
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-07 21:23:24 +0200

    add soft delete interceptor

---
+++ src/Template.Persistence/Interceptors/SoftDeleteInterceptor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Template.Domain.SeedWork;
+
+namespace Template.Persistence.Interceptors
+{
+    public class SoftDeleteInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            MarkEntitiesAsDeleted(eventData);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            MarkEntitiesAsDeleted(eventData);
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private void MarkEntitiesAsDeleted(DbContextEventData eventData)
+        {
+            var deletedEntries = eventData.Context?.ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Deleted)
+                .ToList();
+
+            if (deletedEntries == null || deletedEntries.Count == 0)
+                return;
+
+            foreach (var entry in deletedEntries)
+            {
+                if (entry.Entity is ISoftDelete)
+                {
+                    entry.CurrentValues[nameof(ISoftDelete.IsDeleted)] = true;
+                    entry.CurrentValues[nameof(ISoftDelete.DeletedDate)] = DateTime.UtcNow;
+
+                    entry.State = EntityState.Modified;
+                }
+            }
+        }
+    }
+}
