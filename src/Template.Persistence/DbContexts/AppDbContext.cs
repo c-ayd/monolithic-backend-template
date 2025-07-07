ff6d9979d182534f566e48f4012be1091b094646
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-07 20:31:06 +0200

    add app db context

---
+++ src/Template.Persistence/DbContexts/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Template.Domain.Entities.UserManagement;
+
+namespace Template.Persistence.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        // User management
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<SecurityState> SecurityStates { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
