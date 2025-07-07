4ffc0d018603ef8109cabe5c95d603cbed001b3e
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-07 20:38:04 +0200

    add user profile entity configuration

---
+++ src/Template.Application/Validations/Constants/Entities/ValueObjects/AddressConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Template.Application.Validations.Constants.Entities.ValueObjects
+{
+    public static class AddressConstants
+    {
+        public const int StreetMaxLength = 60;
+        public const int PostalCodeMaxLength = 10;
+        public const int CityMaxLength = 100;
+        public const int CountryMaxLength = 100;
+    }
+}
