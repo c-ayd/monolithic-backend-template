9165687dee669a74ccfbb0c30b161a20cf8e0a08
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-07 20:33:05 +0200

    add security state entity configuration

---
+++ src/Template.Persistence/EntityConfigurations/SecurityStateEntityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Template.Domain.Entities.UserManagement;
+
+namespace Template.Persistence.EntityConfigurations
+{
+    public class SecurityStateEntityConfiguration : IEntityTypeConfiguration<SecurityState>
+    {
+        public void Configure(EntityTypeBuilder<SecurityState> builder)
+        {
+        }
+    }
+}
