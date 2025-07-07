af1db699259b03eb4a0b61d0522d67146cdb1e7a
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-07 21:20:32 +0200

    add updated date interceptor

---
+++ src/Template.Persistence/Interceptors/UpdatedDateInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Template.Domain.SeedWork;
+
+namespace Template.Persistence.Interceptors
+{
+    public class UpdatedDateInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            AddUpdatedDateToEntities(eventData);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            AddUpdatedDateToEntities(eventData);
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private void AddUpdatedDateToEntities(DbContextEventData eventData)
+        {
+            var updatedEntries = eventData.Context?.ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Modified)
+                .ToList();
+
+            if (updatedEntries == null || updatedEntries.Count == 0)
+                return;
+
+            foreach (var entry in updatedEntries)
+            {
+                if (entry.Entity is IUpdateAudit)
+                {
+                    entry.CurrentValues[nameof(IUpdateAudit.UpdatedDate)] = DateTime.UtcNow;
+                }
+            }
+        }
+    }
+}
