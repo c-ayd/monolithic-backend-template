7ba1506f2866dffe5be9663f1f292c0fe23d42e8
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-08 15:05:26 +0200

    add token generator to dependency injection

--- src/Template.Api/Program.cs
+++ src/Template.Api/Program.cs
@@ -1,3 +1,4 @@
+using Template.Infrastructure;
 using Template.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddInfrastructureServices();
 //~ End
 
 var app = builder.Build();
---
+++ src/Template.Infrastructure/ServiceRegistrations.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Template.Application.Abstractions.Crypto;
+using Template.Infrastructure.Crypto;
+
+namespace Template.Infrastructure
+{
+    public static class ServiceRegistrations
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ITokenGenerator, TokenGenerator>();
+        }
+    }
+}
