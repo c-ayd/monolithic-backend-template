d48228e4da680213e2d8e94ef85d475d98027a78
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-13 16:06:34 +0200

    add role dbset

--- src/Template.Persistence/DbContexts/AppDbContext.cs
+++ src/Template.Persistence/DbContexts/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace Template.Persistence.DbContexts
         public DbSet<User> Users { get; set; }
         public DbSet<UserProfile> UserProfiles { get; set; }
         public DbSet<SecurityState> SecurityStates { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
