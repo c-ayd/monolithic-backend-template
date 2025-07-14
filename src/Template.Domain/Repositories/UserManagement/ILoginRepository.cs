f83f56d1c19fa47dcfd3f7c95f282157c4eaff2c
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-14 11:24:22 +0200

    add login repository

---
+++ src/Template.Domain/Repositories/UserManagement/ILoginRepository.cs
@@ -0,0 +1,15 @@
+﻿using Template.Domain.Entities.UserManagement;
+using Template.Domain.SeedWork;
+
+namespace Template.Domain.Repositories.UserManagement
+{
+    public interface ILoginRepository : IRepositoryBase
+    {
+        Task AddAsync(Login newLogin);
+        Task<Login?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<Login?> GetByRefreshTokenAsync(string refreshToken, CancellationToken cancellationToken = default);
+        Task<ICollection<Login>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+        void Delete(Login login);
+        void DeleteAll(IEnumerable<Login> logins);
+    }
+}
---
+++ src/Template.Persistence/Repositories/UserManagement/LoginRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Template.Domain.Entities.UserManagement;
+using Template.Domain.Repositories.UserManagement;
+using Template.Persistence.DbContexts;
+
+namespace Template.Persistence.Repositories.UserManagement
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public LoginRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddAsync(Login newLogin)
+            => await _appDbContext.Logins.AddAsync(newLogin);
+
+        public async Task<Login?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+            => await _appDbContext.Logins
+                .Where(l => l.Id.Equals(id))
+                .FirstOrDefaultAsync(cancellationToken);
+
+        public async Task<Login?> GetByRefreshTokenAsync(string refreshToken, CancellationToken cancellationToken = default)
+            => await _appDbContext.Logins
+                .Where(l => l.RefreshToken == refreshToken)
+                .FirstOrDefaultAsync(cancellationToken);
+
+        public async Task<ICollection<Login>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken = default)
+            => await _appDbContext.Logins
+                .Where(l => l.UserId.Equals(userId))
+                .ToListAsync(cancellationToken);
+
+        public void Delete(Login login)
+            => _appDbContext.Logins.Remove(login);
+
+        public void DeleteAll(IEnumerable<Login> logins)
+            => _appDbContext.Logins.RemoveRange(logins);
+    }
+}
