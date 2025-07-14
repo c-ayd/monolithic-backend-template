30a93ccd6ff3684d29075102cb63ea6c2dad8738
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-14 11:36:17 +0200

    add token repository

---
+++ src/Template.Domain/Repositories/UserManagement/ITokenRepository.cs
@@ -0,0 +1,17 @@
+﻿using Template.Domain.Entities.UserManagement;
+using Template.Domain.Entities.UserManagement.Enums;
+using Template.Domain.SeedWork;
+
+namespace Template.Domain.Repositories.UserManagement
+{
+    public interface ITokenRepository : IRepositoryBase
+    {
+        Task AddAsync(Token newToken);
+        Task<Token?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<Token?> GetByValueAndPurposeAsync(string value, ETokenPurpose purpose, CancellationToken cancellationToken = default);
+        Task<ICollection<Token>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+        Task<ICollection<Token>> GetAllByUserIdAndPurposeAsync(Guid userId, ETokenPurpose purpose, CancellationToken cancellationToken = default);
+        void Delete(Token token);
+        void DeleteAll(IEnumerable<Token> tokens);
+    }
+}
---
+++ src/Template.Persistence/Repositories/UserManagement/TokenRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Template.Domain.Entities.UserManagement;
+using Template.Domain.Entities.UserManagement.Enums;
+using Template.Domain.Repositories.UserManagement;
+using Template.Persistence.DbContexts;
+
+namespace Template.Persistence.Repositories.UserManagement
+{
+    public class TokenRepository : ITokenRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public TokenRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddAsync(Token newToken)
+            => await _appDbContext.Tokens.AddAsync(newToken);
+
+        public async Task<Token?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+            => await _appDbContext.Tokens
+                .Where(t => t.Id.Equals(id))
+                .FirstOrDefaultAsync(cancellationToken);
+
+        public async Task<Token?> GetByValueAndPurposeAsync(string value, ETokenPurpose purpose, CancellationToken cancellationToken = default)
+            => await _appDbContext.Tokens
+                .Where(t => t.Value == value)
+                .FirstOrDefaultAsync(cancellationToken);
+
+        public async Task<ICollection<Token>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken = default)
+            => await _appDbContext.Tokens
+                .Where(t => t.UserId.Equals(userId))
+                .ToListAsync(cancellationToken);
+
+        public async Task<ICollection<Token>> GetAllByUserIdAndPurposeAsync(Guid userId, ETokenPurpose purpose, CancellationToken cancellationToken = default)
+            => await _appDbContext.Tokens
+                .Where(t => t.UserId.Equals(userId) &&
+                    t.Purpose == purpose)
+                .ToListAsync(cancellationToken);
+
+        public void Delete(Token token)
+            => _appDbContext.Tokens.Remove(token);
+
+        public void DeleteAll(IEnumerable<Token> tokens)
+            => _appDbContext.Tokens.RemoveRange(tokens);
+    }
+}
