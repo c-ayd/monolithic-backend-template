b5f69af250c45a5f0f6201b251f0993ab86ea2b8
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-17 18:20:05 +0200

    add settings for templates to fill strings with parameters
    The configuration determines which parameter is used in templates taking parameters

---
+++ src/Template.Infrastructure/Settings/Messaging/Templates/TemplateLinksSettings.cs
@@ -0,0 +1,12 @@
+﻿using Cayd.AspNetCore.Settings;
+
+namespace Template.Infrastructure.Settings.Messaging.Templates
+{
+    public class TemplateLinksSettings : ISettings
+    {
+        public static string SettingsKey => "TemplateLinks";
+
+        public required string EmailVerification { get; set; }
+        public required string ResetPassword { get; set; }
+    }
+}
