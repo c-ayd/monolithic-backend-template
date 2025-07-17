2429bb173e05fba7cff406f479688f1524039e04
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-17 18:16:16 +0200

    add email templates interface for abstraction

---
+++ src/Template.Application/Abstractions/Messaging/Templates/IEmailTemplates.cs
@@ -0,0 +1,10 @@
+﻿using Template.Application.Dtos.Messaging.Templates;
+
+namespace Template.Application.Abstractions.Messaging.Templates
+{
+    public interface IEmailTemplates
+    {
+        EmailTemplateDto GetEmailVerificationTemplate(string token, int expirationTimeInHours);
+        EmailTemplateDto GetPasswordResetTemplate(string token, int expirationTimeInHours);
+    }
+}
---
+++ src/Template.Application/Dtos/Messaging/Templates/EmailTemplateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Template.Application.Dtos.Messaging.Templates
+{
+    public class EmailTemplateDto
+    {
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
