08e7bb6d87edc37fbe3d781b53a8d50e93b199d7
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-18 17:13:40 +0200

    add tests for global exception handler middleware

---
+++ test/IntegrationTests/Template.Test.Integration.Api/Middlewares/GlobalExceptionHandlerTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
+using Template.Test.Integration.Api.Collections;
+using Template.Test.Utility.Fixtures.Hosting;
+
+namespace Template.Test.Integration.Api.Middlewares
+{
+    [Collection(nameof(TestHostCollection))]
+    public class GlobalExceptionHandlerTest
+    {
+        private readonly TestHostFixture _testHostFixture;
+
+        public GlobalExceptionHandlerTest(TestHostFixture testHostFixture)
+        {
+            _testHostFixture = testHostFixture;
+        }
+
+        [Fact]
+        public async Task ExceptionEndpoint_WhenEndpointThrowsException_ShouldReturnCorrectResponse()
+        {
+            // Act
+            var response = await _testHostFixture.Client.GetAsync("/test/exception");
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<GlobalExceptionHandlerResult>(content, new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            // Assert
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+
+            Assert.NotNull(response.Content.Headers.ContentType);
+            Assert.Equal(MediaTypeNames.Application.ProblemJson, response.Content.Headers.ContentType.MediaType);
+
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.Status);
+            Assert.Equal("Internal Server Error", result.Title);
+            Assert.Equal("Something went wrong", result.Detail);
+            Assert.Equal("GET /test/exception",result.Instance);
+            Assert.Equal("Exception", result.Type);
+            Assert.NotNull(result.TraceId);
+        }
+
+        private class GlobalExceptionHandlerResult
+        {
+            public int? Status { get; set; }
+            public string? Title { get; set; }
+            public string? Detail { get; set; }
+            public string? Instance { get; set; }
+            public string? Type { get; set; }
+            public string? TraceId { get; set; }
+        }
+    }
+}
---
+++ test/Template.Test.Utility/Hosting/Endpoints/ExceptionEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Template.Test.Utility.Hosting.Endpoints
+{
+    public static partial class TestEndpoints
+    {
+        public static void AddExceptionEndpoint(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapGet("/test/exception", () =>
+            {
+                throw new Exception("Test exception");
+            });
+        }
+    }
+}
