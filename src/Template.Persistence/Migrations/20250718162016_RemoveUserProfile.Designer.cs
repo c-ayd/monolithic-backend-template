5702e96229cc859325e3a9b291c994a8723f2875
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-18 18:26:59 +0200

    add migration: remove user profile

--- src/Template.Persistence/Migrations/20250718162016_RemoveUserProfile.Designer.cs
+++ src/Template.Persistence/Migrations/20250718162016_RemoveUserProfile.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Net;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Template.Persistence.DbContexts;
@@ -12,9 +13,11 @@ using Template.Persistence.DbContexts;
 namespace Template.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250718162016_RemoveUserProfile")]
+    partial class RemoveUserProfile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
