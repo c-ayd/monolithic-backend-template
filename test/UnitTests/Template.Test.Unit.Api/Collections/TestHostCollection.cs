bde195993ea47a6d917bac603c8033cc52010a56
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-20 14:49:15 +0200

    remove reduntant old test methods

--- test/UnitTests/Template.Test.Unit.Api/Collections/TestHostCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using Template.Test.Utility.Fixtures.Hosting;
-
-namespace Template.Test.Unit.Api.Collections
-{
-    [CollectionDefinition(nameof(TestHostCollection))]
-    public class TestHostCollection : ICollectionFixture<TestHostFixture>
-    {
-    }
-}
