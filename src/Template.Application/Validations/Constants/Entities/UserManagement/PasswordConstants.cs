7d9366baea191210f7f2798cf83ebab4fd757552
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-25 11:59:40 +0200

    add password constants

---
+++ src/Template.Application/Validations/Constants/Entities/UserManagement/PasswordConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Template.Application.Validations.Constants.Entities.UserManagement
+{
+    public static class PasswordConstants
+    {
+        // NOTE: Update the password regex depending on your need as well as the test methods
+        public const string PasswordRegex = @"^(?=.*[A-Za-z])(?=.*\d).{10,}$";
+
+        public const int MinLength = 10;
+        public const int MaxLength = 100;
+    }
+}
