24287cc4ef694224b8e74246203fb6a80d3fcbb4
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-25 12:01:12 +0200

    add guid id generator based on uuidv7

---
+++ src/Template.Persistence/Generators/GuidIdGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Cayd.Uuid;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace Template.Persistence.Generators
+{
+    public class GuidIdGenerator : ValueGenerator<Guid>
+    {
+        public override bool GeneratesTemporaryValues => false;
+        public override Guid Next(EntityEntry entry) => Uuid.V7.Generate();
+    }
+}
---
+++ test/UnitTests/Template.Test.Unit.Persistence/Generators/GuidIdGeneratorTest.cs
@@ -0,0 +1,33 @@
+﻿using Template.Persistence.Generators;
+
+namespace Template.Test.Unit.Persistence.Generators
+{
+    public class GuidIdGeneratorTest
+    {
+        private readonly GuidIdGenerator _generator;
+
+        private readonly int _numberOfGuidsToGenerate = 1000;
+
+        public GuidIdGeneratorTest()
+        {
+            _generator = new GuidIdGenerator();
+        }
+
+        [Fact]
+        public void Next_ShouldGenerateUniqueGuids()
+        {
+            // Arrange
+            var guids = new List<Guid>();
+
+            // Act
+            for (int i = 0; i < _numberOfGuidsToGenerate; ++i)
+            {
+                guids.Add(_generator.Next(null));
+            }
+
+            // Assert
+            var guidSet = guids.ToHashSet();
+            Assert.Equal(guids.Count, guidSet.Count);
+        }
+    }
+}
