e4554b8b364677f099e24547ee09a9bffdaa0497
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-07-25 12:12:43 +0200

    register libraries for validation and cqrs

--- src/Template.Application/ServiceRegistrations.cs
+++ src/Template.Application/ServiceRegistrations.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Template.Application.Validations;
 
 namespace Template.Application
 {
@@ -6,6 +9,15 @@ namespace Template.Application
     {
         public static void AddApplicationServices(this IServiceCollection services)
         {
+            var currentAssembly = Assembly.GetExecutingAssembly()!;
+
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(currentAssembly);
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssembly(currentAssembly);
         }
     }
 }
