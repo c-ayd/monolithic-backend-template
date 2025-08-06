7e8369ceb98fb41b80469eab4fd52a40a9be6a91
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-08-06 23:38:17 +0200

    add health check endpoint

---
+++ src/Template.Api/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Api.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Check()
+        {
+            return Ok();
+        }
+    }
+}
